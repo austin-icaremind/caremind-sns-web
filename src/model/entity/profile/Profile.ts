25c3766f5998823eed8cee07e18fa4a11c3e4d45
박인국 <pkook7@gmail.com>
2023-11-07 15:06:19 +0900

    re

--- src/model/entity/profile/Profile.ts
+++ src/model/entity/profile/Profile.ts
@@ -5,16 +5,35 @@ export class Profile implements ProfileInterface {
   message?: string;
   category?: string;
   data?: Data[];
+  profileImage?: string;
+  name?: string;
+  location?: string;
+  jobDescription?: string;
+  connections?: number;
 
-  constructor(id: number, message: string, category: string, data: Data[]) {
+  constructor(
+    profileImage: string,
+    name: string,
+    location: string,
+    jobDescription: string,
+    connections: number,
+    id: number,
+    message: string,
+    category: string,
+    data: Data[]
+  ) {
+    this.profileImage = profileImage;
+    this.name = name;
+    this.location = location;
+    this.jobDescription = jobDescription;
+    this.connections = connections;
     this.id = id;
     this.message = message;
     this.category = category;
-    this.data = data;
+    this.data = data; // Education 데이터를 배열에 할당
   }
 }
 
-// Education 데이터를 나타내는 인터페이스 정의
 export interface Data {
   imgSrc: string;
   title: string;
